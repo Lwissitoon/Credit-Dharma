e98590a545969fedea74f38eb1a5a1aa32c6629f
Luis Mateo <luismateo9594@gmail.com>
2021-11-19 23:45:32 -0400

    Manejo de usuarios y permisos

---
+++ Credit Dharma/Controllers/UsuariosController.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Credit_Dharma.Data;
+using UserManager.Models;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Credit_Dharma.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly Credit_DharmaContext _context;
+
+        public UsuariosController(Credit_DharmaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Usuarios
+        public async Task<IActionResult> Index()
+        {
+            if (TempData["Loggedin"]==null)
+            {
+                TempData["Loggedin"] = "No";
+            }
+
+            if (TempData["Loggedin"].ToString().ToUpper()=="SI" )
+            {
+               
+                if (TempData["Admin"].ToString().ToUpper() == "SI")
+                {
+                    return View(await _context.Usuario.ToListAsync());
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        // GET: Usuarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuario
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserId,Username,Name,Lastname,Role,Password")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(usuario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuario.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UserId,Username,Name,Lastname,Role,Password")] Usuario usuario)
+        {
+            if (id != usuario.UserId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(usuario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UsuarioExists(usuario.UserId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuario
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var usuario = await _context.Usuario.FindAsync(id);
+            _context.Usuario.Remove(usuario);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UsuarioExists(int id)
+        {
+            return _context.Usuario.Any(e => e.UserId == id);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> Signin(string Username, string Password)
+        {
+
+          //  Response.WriteAsync(await _context.Usuario.ToListAsync().f(m=>m.Username=="").Result.ToString());
+
+            
+            TempData.Clear();
+            //   Response.WriteAsync(Username + Password);()
+            var login = await _context.Usuario.ToListAsync();
+
+           var  usuario = login.FirstOrDefault(m => m.Username.ToUpper().Trim() == Username.ToUpper().Trim() && m.Password.ToUpper().Trim() == Password.ToUpper().Trim());
+            //  .FirstOrDefault(m => m.Username.ToUpper().Trim() == Username.ToUpper().Trim() && m.Password.ToUpper().Trim() == Password.ToUpper().Trim()).Result;
+            //Response.WriteAsync(usuario.First().Username+ usuario.First().Role);
+            if (usuario.Username != null)
+            {
+                //  var role = _context.Usuario.FirstOrDefaultAsync(m => m.Username == Username);
+                TempData["Loggedin"] = "Si";
+                TempData["Username"] = usuario.Username;
+                TempData["Role"] = usuario.Role;
+                if (usuario.Role.ToString().ToUpper().Trim() == "Admin".ToUpper().Trim())
+                {
+                    TempData["Admin"] = "Si";
+                    return RedirectToAction("Index");
+                }
+               else
+                {
+                    TempData["Admin"] = "No";
+                    return RedirectToAction("Index", "Home");
+                }
+
+            }
+            else
+            {
+                TempData["Loggedin"] = "No";
+                return RedirectToAction("Login");
+            }
+        }
+
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+    }
+}
--- Credit Dharma/Startup.cs
+++ Credit Dharma/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Credit_Dharma.Data;
 
 namespace Credit_Dharma
 {
@@ -24,6 +26,9 @@ namespace Credit_Dharma
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<Credit_DharmaContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("Credit_DharmaContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
