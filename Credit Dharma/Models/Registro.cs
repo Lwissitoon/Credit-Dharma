551a7ffdc3301d87d73200077f06c41c0ac79395
Luis Mateo <luismateo9594@gmail.com>
2021-11-29 01:27:03 -0400

    -Se corrigieron los nombres de los registros -Se cambio el nombre de  la paginade registros -Se actualizo la interfaz de detalles registro

--- Credit Dharma/Models/Registro.cs
+++ Credit Dharma/Models/Registro.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,10 +10,15 @@ namespace Credit_Dharma.Models
     public class Registro
     {
         [Key]
+        [DisplayName("Numero de identificacion")]
         public int IdNotification { get; set; }
+        [DisplayName("Fecha Registro")]
         public string NotificationDate { get; set; }
+        [DisplayName("Cuenta del cliente")]
         public string UserAccountNumber { get; set; }
+        [DisplayName("Detalles")]
         public string NotificationDetails { get; set; }
+        [DisplayName("Realizada por")]
         public string Username { get; set; }
 
     }
